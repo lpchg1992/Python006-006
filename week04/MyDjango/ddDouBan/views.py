9b2f9fd808cbf88afa675e718e1eb877e616e6b9
name <email>
2021-01-09 21:27:31 +0800

    objects调度器筛查处理数据

--- week04/MyDjango/ddDouBan/views.py
+++ week04/MyDjango/ddDouBan/views.py
@@ -5,20 +5,24 @@ from django.db.models import Avg
 
 
 def books_short(request):
-    # 从models取数据传给template
+    # 从models取数据传给template【queryset数据类型可以用python方法处理】
     shorts = T1.objects.all()
     # 评论数量
     counter = T1.objects.all().count()
 
     # 平均星级
     # star_value = T1.objects.values('n_star')
+    # 聚合函数返回字典
     star_avg = f"{T1.objects.aggregate(Avg('n_star'))['n_star__avg']:0.1f}"
     # 情感倾向
     sent_avg = f"{T1.objects.aggregate(Avg('sentiment'))['sentiment_avg']:0.2f}"
 
     # 正向数量
     queryset = T1.objects.values('sentiment')
+    # 双下划线前面：字段，后面：判断条件：
+    # 判断条件通常有：gte:>=   gt:>  lt:<  lte:<=
     conditons = {'sentiment__gte': 0.5}
+    # 过滤
     plus = queryset.filter(**conditons).count()
 
     # 负向数量
