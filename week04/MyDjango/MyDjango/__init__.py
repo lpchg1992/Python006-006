35121e79be09a5f8020f3b0ae51d5080a38e6d6b
name <email>
2021-01-09 18:36:59 +0800

    orm创建数据表

---
+++ week04/MyDjango/MyDjango/__init__.py
@@ -0,0 +1,3 @@
+# import pymysql
+# # 进行初始化，替换原始的mysqldb
+# pymysql.install_as_MySQLdb()
--- week04/MyDjango/index/models.py
+++ week04/MyDjango/index/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+# 图书 or 电影
+class Type(models.Model):
+    # id = models.AutoField(primary_key=True) # Django自动创建并设置为主键
+    typename = models.CharField(max_length=20)
+
+
+# 作品名称作者主演
+class name(models.Model):
+    # id 自动创建
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    stars = models.CharField(max_length=5)
